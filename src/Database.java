9888ceb7358d014f48d64a4b4385b23232786521
Jean-Leherle <j.leherle@gmail.com>
2022-10-28 16:43:34 +0200

    :bug: fix : answer to all bad notification from IDE, add a return to the database query

--- src/Database.java
+++ src/Database.java
@@ -1,43 +1,58 @@
 import java.sql.*;
 
 public class Database {
-    public static void main(String[] args){
+
+    //test avec une bdd déjà existante
+    private static final String url = "jdbc:postgresql://localhost:5432/zeng";
+    private static final String user = "zeng";
+    private static final String passwd = "zeng";
+
+            /*
+                url = "jdbc:postgresql://localhost:5432/la_fontaine";
+                String user = "la_fontaine";
+                String passwd = "jean";
+            */
+
+    protected static ResultSet sendRequest(String request){
         //modèle et source d'information : https://www.jmdoudoux.fr/java/dej/chap-jdbc.htm
         try {
-            //instalation nécessaire au préalable
+            //installation nécessaire au préalable
             Class.forName("org.postgresql.Driver");
             System.out.println("Driver ok");
 
 
-
-            //test avec une bdd déjà existante
-            String url = "jdbc:postgresql://localhost:5432/zeng";
-            String user = "zeng";
-            String passwd = "zeng";
-
             Connection conn = DriverManager.getConnection(url, user, passwd);
             System.out.println("Connexion effective !");
-            ResultSet resultats = null;
-            String requete = "Select * FROM sheet;";
+            ResultSet results;
             try {
                 Statement stmt = conn.createStatement();
-                resultats = stmt.executeQuery(requete);
-
-                ResultSetMetaData rsmd = resultats.getMetaData();
+                results = stmt.executeQuery(request);
+                return results;
+                /*
+                ResultSetMetaData rsmd = results.getMetaData();
                 int nbCols = rsmd.getColumnCount();
-                while (resultats.next()) {
-                    for (int i = 1; i <= nbCols; i++)
-                        System.out.println(resultats.getString(i) + " ");
+                results.close();
+
+                while (results.next()) {
+                for (int i = 1; i <= nbCols; i++)
+                    System.out.println(results.getString(i) + " ");
                 }
-                resultats.close();
+                */
+
+
             } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
                 //traitement de l'exception
             }
 
 
         }catch (Exception e){
             e.printStackTrace();
+            return null;
         }
+
+
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
 //todo  : avoir une base d'objet lié au poeme fonctionnel : génération possible
 //todo : savoir lié une bdd à java
-// todo : créer un serveur fonctionel avec une route pour poster des vers et une route pour rédiger un poeme.
+//todo : créer un serveur fonctionel avec une route pour poster des vers et une route pour rédiger un poeme.
     public static void main(String[] args){
         System.out.println("Hello World");
 
@@ -17,5 +20,23 @@ public class Main {
         Feet feet1 = new Feet("La lune est belle", 5, "el", theme, "fin");
 
         feet1.showFeet();
+
+        ResultSet results = Database.sendRequest("Select * FROM sheet limit 2;");
+
+        ResultSetMetaData rsmd;
+        try {
+            assert results != null;
+            rsmd = results.getMetaData();
+            int nbCols = rsmd.getColumnCount();
+            results.close();
+            while(results.next()){
+                for (int i = 1; i <= nbCols; i++)
+                    System.out.println(results.getString(i) + " ");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
